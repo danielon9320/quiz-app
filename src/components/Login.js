e2ace3dc22fd62069397a2f0fa426535cb21c6f7
danielon9320 <daniel_ls93@live.com.ar>
2023-01-16 21:59:25 -0300

    validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,36 @@
 import React from "react";
 import { TextField, Button, Box, Card, CardContent, Typography } from "@mui/material";
 import Center from './Center'
+import useForm from '../hooks/useForm'
+
+const getFreshModel = () => ({
+  name:'',
+  email: ''
+})
 
 export default function Login() {
+
+  const {         values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange } = useForm(getFreshModel);
+
+    const login = e => {
+      e.preventDefault();
+      if(validate())
+        console.log(values)
+    }
+
+    const validate = () =>{
+      let temp = {}
+      temp.email = (/\S+@\S+\.\S+/).test(values.email)?"":"Email isn't valid"
+      temp.name = values.name !==""?"":"Mandatory field"
+      setErrors(temp)
+      return Object.values(temp).every(x => x === "")
+    }
+
+
   return (
     <Center>
     <Card sx={{width:'400'}}>
@@ -14,9 +42,9 @@ export default function Login() {
         width:'90%'
       }
     }}>
-      <form noValidate autoComplete="off">
-        <TextField label="Email" name="email" variant="outlined" />
-        <TextField label="Name" name="name" variant="outlined" />
+      <form noValidate autoComplete="off" onSubmit={login}>
+        <TextField label="Email" name="email" value={values.email} onChange={handleInputChange} variant="outlined" {...(errors.email && {error:true, helperText:errors.email})}/>
+        <TextField label="Name" name="name" value={values.name} onChange={handleInputChange} variant="outlined" {...(errors.name && {error:true, helperText:errors.name})}/>
         <Button type="submit" variant="contained" size="large" sx={{width:'90%'}}>
           Start{" "}
         </Button>
