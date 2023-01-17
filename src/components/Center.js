3215fee4fe497d80c7c529bf6b3f63e0152c20cd
danielon9320 <daniel_ls93@live.com.ar>
2023-01-16 21:09:01 -0300

    ui login page almost done

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Grid} from '@mui/material'
 
-export default function Center() {
+export default function Center(props) {
   return (
     <Grid container
     direction="column"
     alignItems="center"
     justifyContent="center"
-    sx={{minHeight='100vh'}}>
+    sx={{minHeight : '100vh'}}>
     <Grid item xs={1}>
+    {props.children}
     </Grid>
     </Grid>
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { TextField, Button, Box, Card, CardContent } from "@mui/material";
-import Center from './components/Center'
+import { TextField, Button, Box, Card, CardContent, Typography } from "@mui/material";
+import Center from './Center'
 
 export default function Login() {
   return (
     <Center>
-        <Card sx={{width:'400'}}>
-     <CardContent>
+    <Card sx={{width:'400'}}>
+     <CardContent sx={{textAlign: 'center'}}>
+     <Typography variant="h3" sx={{my:3}}>Quiz App</Typography>
      <Box sx={{
       '& .MuiTextField-root':{
         m: 1,
@@ -24,6 +25,8 @@ export default function Login() {
      </CardContent>
     </Card>
     </Center>
+
+    
 
  
   );
